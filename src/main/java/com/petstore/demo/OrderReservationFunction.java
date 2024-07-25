48bfe4133d2d1f4477b6d9171b54f6eb016b1746
Maryna Maroz <maryna_maroz@epam.com>
2024-07-25 13:30:42 +0200

    feat: service bus trigger function

--- src/main/java/com/petstore/demo/OrderReservationFunction.java
+++ src/main/java/com/petstore/demo/OrderReservationFunction.java
@@ -1,7 +1,6 @@
 package com.petstore.demo;
 
 import com.azure.storage.blob.BlobClient;
-import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
 import com.microsoft.azure.functions.*;
@@ -9,43 +8,29 @@ import com.microsoft.azure.functions.annotation.*;
 
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.Optional;
 
 /**
- * Azure Functions with HTTP Trigger.
+ * Azure Functions with Service Bus Trigger.
  */
 public class OrderReservationFunction {
 
     private static final String connection = System.getenv("AZURE_STORAGE_CONNECTION");
+    private static final String containerName = System.getenv("AZURE_STORAGE_CONTAINER_NAME");
     private static final BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connection).buildClient();
-    private static final BlobContainerClient blobContainerClient = blobServiceClient.getBlobContainerClient("orders");
 
     @FunctionName("OrderItemReservation")
     @StorageAccount("AzureWebJobsStorage")
-    public HttpResponseMessage run(
-            @HttpTrigger(
-                    name = "req",
-                    methods = {HttpMethod.GET, HttpMethod.POST},
-                    authLevel = AuthorizationLevel.ANONYMOUS)
-            HttpRequestMessage<Optional<OrderReservationRequest>> request,
-            final ExecutionContext context) {
-        context.getLogger().info("Java HTTP trigger processed an Order Reservation request.");
+    @FixedDelayRetry(maxRetryCount = 3, delayInterval = "00:00:10")
+    public void serviceBusProcess(
+            @ServiceBusQueueTrigger(name = "orders",
+                    queueName = "orders",
+                    connection = "AzureWebJobsServiceBus") OrderReservationRequest message,
+            final ExecutionContext context
+    ) {
+        context.getLogger().info("Java Service Bus Queue trigger function processed a message: " + message);
 
-        if (!request.getBody().isPresent()) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST)
-                    .body("Invalid order request object.")
-                    .build();
-        }
-
-        var orderRequest = request.getBody().get();
-        var sessionId = orderRequest.getSessionId();
-        var payload = orderRequest.getPayload();
-
-        if (sessionId == null || payload == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).
-                    body("SessionId or payload is missing.")
-                    .build();
-        }
+        var sessionId = message.getSessionId();
+        var payload = message.getPayload();
 
         context.getLogger().info("Session ID: " + sessionId);
         context.getLogger().info("Order Reservation payload: " + payload);
@@ -53,12 +38,14 @@ public class OrderReservationFunction {
         context.getLogger().info("Trying to save Order json in Blob Storage.");
 
         // Save order json to blob storage
-        BlobClient blobClient = blobContainerClient.getBlobClient(sessionId + ".json");
-        byte[] blobData = payload.getBytes(StandardCharsets.UTF_8);
-        blobClient.upload(new ByteArrayInputStream(blobData), blobData.length, true);
-
-        return request.createResponseBuilder(HttpStatus.OK)
-                .body("Session Id: " + sessionId + ", payload: " + payload)
-                .build();
+        try {
+            var blobContainerClient = blobServiceClient.getBlobContainerClient(containerName);
+            BlobClient blobClient = blobContainerClient.getBlobClient(sessionId + ".json");
+            byte[] blobData = payload.getBytes(StandardCharsets.UTF_8);
+            blobClient.upload(new ByteArrayInputStream(blobData), blobData.length, true);
+        } catch (Exception e) {
+            context.getLogger().severe("Error during uploading to Blob Storage: " + e);
+            throw new RuntimeException("Failed upload to the storage", e);
+        }
     }
 }
